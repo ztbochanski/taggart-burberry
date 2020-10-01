1b2f9f1dcf5a26f71d717db8c1ac8b63bae84001
zach <ztbochanski@gmail.com>
2020-10-01 08:29:39 -0700

    shop actions, reducer, types, HOC spinner for loading, firebase utils

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,4 @@
 import { createSelector } from "reselect";
-import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
 
@@ -8,15 +7,13 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
-// convert select collections object into an array to be displayed by the preview component
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  (collections) => Object.keys(collections).map((key) => collections[key])
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
 );
 
-export const selectCollection = memoize((collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => collections[collectionUrlParam]
-  )
-);
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
+  );
